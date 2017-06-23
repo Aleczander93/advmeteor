c24295b8baa32b0c2a7864fd0f5ff87022f3d4d2
aleczander <afigueroa81@ufl.edu>
2017-06-22 23:24:27 -0400

    fixing environment

---
+++ both/router/router.js
@@ -0,0 +1,11 @@
+FlowRouter.route('/', {
+  action:function(){
+    FlowLayout.render('layout', {sidebar:'sidebar', main:'home', cart:'cart'});
+  }
+});
+
+FlowRouter.route('/category/:categoryName',{
+  action:function(){
+    FlowLayout.render('layout', {sidebar:'sidebar', main:'category', cart:'cart'});
+  }
+});
